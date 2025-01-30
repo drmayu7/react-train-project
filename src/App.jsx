f0b1341e421d86788aaadda68a3d0b8858c6d74f
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-01-31 07:44:03 +0800

    conditional rendering - if

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,30 @@ import {EXAMPLES} from "./data.js";
 
 
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState('components');
+    // Dynamically render the selected topic
+    const [selectedTopic, setSelectedTopic] = useState();
 
     function handleSelect(selectedButton) {
         setSelectedTopic(selectedButton);
         console.log(selectedTopic)
     }
+
+    // conditionally render the selected topic before and after selection
+    let tabContent = <p> Please select a topic.</p>;
+    if (selectedTopic) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>
+                        {EXAMPLES[selectedTopic].code}
+                    </code>
+                </pre>
+            </div>
+        )
+    }
+
 
     return (<div>
             <Header/>
@@ -38,15 +56,7 @@ function App() {
                         <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                         <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                     </menu>
-                    <div id='tab-content'>
-                        <h3>{EXAMPLES[selectedTopic].title}</h3>
-                        <p>{EXAMPLES[selectedTopic].description}</p>
-                        <pre>
-                            <code>
-                            {EXAMPLES[selectedTopic].code}
-                        </code>
-                        </pre>
-                    </div>
+                    {tabContent}
                 </section>
             </main>
         </div>
