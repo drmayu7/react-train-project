d6353d6b2acb88e39ad1031ea5db597e2b75c339
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-01-31 08:17:13 +0800

    dynamic css styling - highlights when selected

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -2,10 +2,10 @@
 //     return <li><button>{props.children}</button></li>; // children is a special prop that contains the content between the opening and closing tags of the component
 // }
 
-export default function TabButton({children,onSelect}) {
+export default function TabButton({children,onSelect,isSelected}) {
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
         </li>
     );
 }
