7695c8688d09d1a2bfc96dd8835035d17f3e85c0
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-01-31 09:17:32 +0800

    output list data dynamically - prevent key error in console

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
-                        {CORE_CONCEPTS.map((conceptItem)=> (<CoreConcept {...conceptItem} />))}
+                        {CORE_CONCEPTS.map((conceptItem)=> (<CoreConcept key={conceptItem.title} {...conceptItem} />))}
                         {/*<CoreConcept*/}
                         {/*    title={CORE_CONCEPTS[0].title}*/}
                         {/*    description={CORE_CONCEPTS[0].description}*/}
